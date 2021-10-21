d1f93faf6bff0ad026ae3b3772e27b696809f6f5
StevenD <dorvilsteven@gmail.com>
2021-10-21 12:19:45 -0400

    fixed room number

--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -1,7 +1,8 @@
 const Manager = require('../lib/Manager');
 
 test('creates manager object', () => {
-    const manager = new Manager('marcus');
+    const manager = new Manager('marcus', 111);
+    // const manager = new Manager('marcus', parseInt('111'));
 
     expect(manager.name).toBe('marcus');
     expect(manager.id).toEqual(expect.any(Number));
