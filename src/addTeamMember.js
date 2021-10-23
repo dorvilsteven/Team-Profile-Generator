6b2e24df6b84b4c34c6b57ef96d8474255bc633e
StevenD <dorvilsteven@gmail.com>
2021-10-23 17:19:54 -0400

    init manager and team

--- src/addTeamMember.js
+++ src/addTeamMember.js
@@ -1,8 +1,3 @@
-const Manager = require('../lib/Manager');
-const Engineer = require('../lib/Engineer');
-const Intern = require('../lib/Intern');
-const Team = require('../lib/Team');
-
 module.exports = (employee, team) => {
     team.addMember(employee);
     return team;
--- src/initManager.js
+++ src/initManager.js
@@ -1,7 +1,5 @@
 const Inquirer = require('inquirer');
 const Manager = require('../lib/Manager');
-const Engineer = require('../lib/Engineer');
-const Intern = require('../lib/Intern');
 const Team = require('../lib/Team');
 const addTeamMember = require('./addTeamMember');
 
